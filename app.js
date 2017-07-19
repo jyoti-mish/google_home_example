8cdc765d348ad9ae07f59c2b1c9369ee5051dc24
jyoti-mish <jyoti12.mishra@gmail.com>
2017-07-19 15:46:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,15 +21,29 @@ let bodyParser = require('body-parser');
 let app = express();
 app.use(bodyParser.json({type: 'application/json'}));
 
-const Temp_ACTION = 'SettempIntent';
-const status_ACTION = 'SetStatusIntent';
-const SETPOINT_ARGUMENT = 'SetPoint';
-const NUMBER_ARGUMENT = 'Temp';
-
 // [START YourAction]
 app.post('/', function (req, res) {
-  
-  assistant.tell('Alright, your silly name is ');
+  const assistant = new Assistant({request: req, response: res});
+  console.log('Request headers: ' + JSON.stringify(req.headers));
+  console.log('Request body: ' + JSON.stringify(req.body));
+
+  // Fulfill action business logic
+  function responseHandler (assistant) {
+    // Complete your fulfillment logic and send a response
+    assistant.tell('Hello, World!');
+  }
+
+  assistant.handleRequest(responseHandler);
+});
+// [END YourAction]
+
+if (module === require.main) {
+  // [START server]
+  // Start the server
+  let server = app.listen(process.env.PORT || 8080, function () {
+    let port = server.address().port;
+    console.log('App listening on port %s', port);
+  });
   // [END server]
 }
 
